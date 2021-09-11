828c7856caf52edc3a4815959dd6feda59151359
Eric02851 <ziggy2851@gmail.com>
2021-09-10 22:40:55 -0700

    Working google sign in button in main

--- boardScripts/main.js
+++ boardScripts/main.js
@@ -109,6 +109,14 @@ const postData = () => {
     }));
 }
 
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
 updateStatus()
 loadBoard("rnbqkbnr/pp1ppppp/8/2p5/2P5/8/PP1PPPPP/RNBQKBNR w KQkq c6 0 2")
 
