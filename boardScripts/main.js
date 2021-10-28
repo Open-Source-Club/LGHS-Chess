a360794d65bb25cd8d058ff014ace94f9afb3e8a
AnthonyFurman <71467649+AnthonyFurman@users.noreply.github.com>
2021-10-27 19:22:15 -0700

    fix

--- boardScripts/main.js
+++ boardScripts/main.js
@@ -74,18 +74,7 @@ const loadData = () => {
             onSnapEnd: onSnapEnd
         })
 
-        let myPromise = new Promise(function(myResolve, myReject) {
-        // "Producing Code" (May take some time)
-
-          myResolve(); // when successful
-          myReject();  // when error
-        });
-
-        // "Consuming Code" (Must wait for a fulfilled Promise)
-        myPromise.then(
-          function(value) { /* code if successful */ },
-          function(error) { /* code if some error */ }
-        );
+        countdown();
 
         updateStatus()
     };
